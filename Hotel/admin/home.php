51abaa87830d127354d5d68af6c91892ca725c12
SushanthEnukonda <enukondasushanthreddy21@gmail.com>
2021-07-12 19:12:37 +0530

    final commit

---
+++ Hotel/admin/home.php
@@ -0,0 +1,217 @@
+﻿<?php  
+session_start();  
+if(!isset($_SESSION["user"]))
+{
+ header("location:index.php");
+}
+?> 
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Administrator	</title>
+    
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    
+    <link href="assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+   
+    <link href="assets/css/custom-styles.css" rel="stylesheet" />
+    
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default top-navbar" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="home.php"> <?php echo $_SESSION["user"]; ?> </a>
+            </div>
+
+        </nav>
+        <!--/. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+
+                    <li>
+                        <a class="active-menu" href="home.php">Status</a>
+                    </li>
+                    
+                        <a href="logout.php">Logout</a>
+                    </li>
+                   
+
+
+                    
+				</ul>
+
+            </div>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper">
+            <div id="page-inner">
+
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-header">
+                            Status <small>Room Booking </small>
+                        </h1>
+                    </div>
+                </div>
+                <!-- /. ROW  -->
+				<?php
+						include ('db.php');
+						$sql = "select * from roombook";
+						$re = mysqli_query($con,$sql);
+						$c =0;
+						while($row=mysqli_fetch_array($re) )
+						{
+								$new = $row['stat'];
+								$cin = $row['cin'];
+								$id = $row['id'];
+								if($new=="Not Confirm")
+								{
+									$c = $c + 1;
+									
+								
+								}
+						
+						}
+						
+									
+									
+
+						
+				?>
+
+				<div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            
+                        </div>
+                        <div class="panel-body">
+                            <div class="panel-group" id="accordion">
+							
+							<div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+											<button class="btn btn-default" type="button">
+												 New Room Bookings
+											</button>
+											</a>
+                                        </h4>
+                                    </div>
+                                    <div id="collapseTwo" class="panel-collapse in" style="height: auto;">
+                                        <div class="panel-body">
+                                           <div class="panel panel-default">
+                        
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                           
+                                            <th>Name</th>
+                                            <th>Email</th>
+                                            <th>Country</th>
+											<th>Room</th>
+											<th>Bedding</th>
+											<th>Meal</th>
+											<th>Check In</th>
+											<th>Check Out</th>
+											<th>Status</th>
+											<th>More</th>
+											
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        
+									<?php
+									$tsql = "select * from roombook";
+									$tre = mysqli_query($con,$tsql);
+									while($trow=mysqli_fetch_array($tre) )
+									{	
+										$co =$trow['stat']; 
+										
+											echo"<tr>
+												
+												<th>".$trow['FName']." ".$trow['LName']."</th>
+												<th>".$trow['Email']."</th>
+												<th>".$trow['Country']."</th>
+												<th>".$trow['TRoom']."</th>
+												<th>".$trow['Bed']."</th>
+												<th>".$trow['Meal']."</th>
+												<th>".$trow['cin']."</th>
+												<th>".$trow['cout']."</th>
+												<th>".$trow['stat']."</th>
+												
+												<th><a href='roombook.php?rid=".$trow['id']." ' class='btn btn-primary'>Action</a></th>
+												</tr>";
+										
+									
+									}
+									?>
+                                        
+                                    </tbody>
+                                </table>
+								
+                            </div>
+                        </div>
+                    </div>
+                      <!-- End  Basic Table  --> 
+                                        </div>
+                                    </div>
+                                </div>
+								
+										
+                                        
+										   
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+				
+				<!--DEMO END-->
+				
+										
+                    
+
+                <!-- /. ROW  -->
+				
+            </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+        <!-- /. PAGE WRAPPER  -->
+    </div>
+    <!-- /. WRAPPER  -->
+    <!-- JS Scripts-->
+    <!-- jQuery Js -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- Bootstrap Js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Metis Menu Js -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+    <!-- Morris Chart Js -->
+    <script src="assets/js/morris/raphael-2.1.0.min.js"></script>
+    <script src="assets/js/morris/morris.js"></script>
+    <!-- Custom Js -->
+    <script src="assets/js/custom-scripts.js"></script>
+
+
+</body>
+</html>
---
+++ Hotel/index.php
@@ -0,0 +1,450 @@
+<?php
+include('db.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>THE HOTEL</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Resort Inn Responsive , Smartphone Compatible web template , Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen">
+<link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+<link rel="stylesheet" href="css/jquery-ui.css" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/modernizr-2.6.2.min.js"></script>
+
+</head>
+<body>
+<!-- header -->
+<div class="banner-top">
+			
+			<div class="clearfix"></div>
+		</div>
+	<div class="w3_navigation">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<div class="navbar-header navbar-left">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<h1><a class="navbar-brand" href="index.php">THE HOTEL</a></h1>
+				</div>
+				
+				<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+					<nav class="menu menu--iris">
+						<ul class="nav navbar-nav menu__list">
+							<li class=""><a href="" class="">Home</a></li>
+							<li class=""><a href="#gallery" class="">Gallery</a></li>
+							<li class=""><a href="#rooms" class="">Rooms</a></li>
+							<li class=""><a href="#contact" class="">Contact Us</a></li>
+						</ul>
+					</nav>
+				</div>
+			</nav>
+
+		</div>
+	</div>
+<!-- //header -->
+		<!-- banner -->
+	<div id="home" class="w3ls-banner">
+		
+		<div class="slider">
+			<div class="callbacks_container">
+				<ul class="rslides callbacks callbacks1" id="slider4">
+					<li>
+						<div class="w3layouts-banner-top">
+
+							<div class="container">
+								<div class="agileits-banner-info">
+								<h4  style="color: white !important;">THE HOTEL</h4>
+									 
+								</div>	
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="w3layouts-banner-top w3layouts-banner-top1">
+							<div class="container">
+								<div class="agileits-banner-info">
+								<h4  style="color: white !important;">THE HOTEL</h4>
+									
+								</div>	
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="w3layouts-banner-top w3layouts-banner-top2">
+							<div class="container">
+								<div class="agileits-banner-info">
+								<h4  style="color: white !important;">THE HOTEL</h4>
+								
+								</div>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+			
+		</div>
+		   
+
+			
+
+<br><br><br><br><div class="advantages">
+	<div class="container">
+		<div class="advantages-main">
+				<h3 class="title-w3-agileits">Services</h3>
+		   <div class="">
+			 <div class="col-md-6" >
+			 	<div class="advantage-block ">
+					
+			 		<h4>Lorem ipsum dolor sit amet</h4>
+			 		
+					<p>Lorem ipsum dolor sit amet</p>
+					<p>Lorem ipsum dolor sit amet</p>
+			 		
+			 	</div>
+			 </div>
+			 <div class="col-md-6" >
+			 	<div class="advantage-block">
+					
+			 		<h4>Lorem ipsum dolor sit amet</h4>
+			 		
+					<p>Lorem ipsum dolor sit amet</p>
+					<p>Lorem ipsum dolor sit amet</p>
+			 	</div>
+			 </div>
+			<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<section class="portfolio-w3ls" id="gallery">
+		 <h3 class="title-w3-agileits title-black-wthree">Gallery</h3>
+				<div class="col-md-3 gallery-grid gallery1">
+					<img src="images/gal1.jpeg" class="img-responsive" alt="/">
+						
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<img src="images/gal2.jpeg" class="img-responsive" alt="/">
+						
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<img src="images/gal3.jpeg" class="img-responsive" alt="/">
+						
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+				<img src="images/gal4.jpeg" class="img-responsive" alt="/">
+						
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<img src="images/gal5.jpeg" class="img-responsive" alt="/">
+						
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<img src="images/gal6.jpeg" class="img-responsive" alt="/">
+						
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<img src="images/gal7.jpeg" class="img-responsive" alt="/">
+						
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<img src="images/gal8.jpeg" class="img-responsive" alt="/">
+						
+				</div>
+					<div class="col-md-3 gallery-grid gallery1">
+					<img src="images/gal9.jpeg" class="img-responsive" alt="/">
+						
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<img src="images/g10.jpg" class="img-responsive" alt="/">
+						
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<img src="images/gal3.jpeg" class="img-responsive" alt="/">
+						
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<img src="images/gal1.jpeg" class="img-responsive" alt="/">
+						
+				</div>
+				<div class="clearfix"> </div>
+</section>
+
+      <div class="plans-section" id="rooms">
+				 <div class="container">
+				 <h3 class="title-w3-agileits title-black-wthree">Rooms</h3>
+						<div class="priceing-table-main">
+				 <div class="col-md-3 price-grid">
+					
+						<div class="price-gd-top">
+						<img src="images/deluxe.jpeg" alt=" " class="img-responsive" />
+							<h4>Deluxe Room</h4>
+						</div>
+						<div class="price-gd-bottom">
+							   
+							<div class="price-selet">	
+								<h3>Rs 2500</h3>						
+								<a href="admin/reservation.php" >Book Now</a>
+							</div>
+						</div>
+					
+				</div>
+				<div class="col-md-3 price-grid ">
+					
+						<div class="price-gd-top">
+						<img src="images/luxury.jpeg" alt=" " class="img-responsive" />
+							<h4>Luxury Room</h4>
+						</div>
+						<div class="price-gd-bottom">
+							
+							<div class="price-selet">
+								<h3>Rs 2000</h3>
+								<a href="admin/reservation.php" >Book Now</a>
+							</div>
+						</div>
+					
+				</div>
+				<div class="col-md-3 price-grid lost">
+					
+						<div class="price-gd-top">
+						<img src="images/guest_house.jpeg" alt=" " class="img-responsive" />
+							<h4>Guest House</h4>
+						</div>
+						<div class="price-gd-bottom">
+							
+							<div class="price-selet">
+								<h3>Rs 1500</h3>
+								<a href="admin/reservation.php" >Book Now</a>
+							</div>
+						</div>
+					
+				</div>
+				<div class="col-md-3 price-grid wthree lost">
+					
+						<div class="price-gd-top ">
+							<img src="images/single.jpeg" alt=" " class="img-responsive" />
+							<h4>Single Room</h4>
+						</div>
+						<div class="price-gd-bottom">
+							
+							<div class="price-selet">
+								<h3>Rs 1000</h3>
+								<a href="admin/reservation.php" >Book Now</a>
+							</div>
+						</div>
+					
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	 
+	<div class="w3l-visitors-agile" >
+		<div class="container">
+                 <h3 class="title-w3-agileits title-black-wthree">reviews</h3> 
+		</div>
+		<div class="w3layouts_work_grids">
+			<section class="slider">
+				<div class="flexslider">
+					<ul class="slides">
+						<li>
+							
+							<div class="w3layouts_work_grid_right">
+								<h4>
+								
+								Worth to come again
+								</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+								<h5>person 1</h5>
+								
+							<div class="clearfix"> </div>
+						</li>
+						<li>
+							
+							<div class="w3layouts_work_grid_right">
+								<h4>
+								
+								Worth to come again
+								</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+								labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+								 nisi ut aliquip ex ea commodo consequat. </p>
+								<h5>person 2</h5>
+								
+							</div>
+							<div class="clearfix"> </div>
+						</li>
+						<li>
+							
+							<div class="w3layouts_work_grid_right">
+								<h4>
+								
+								Worth to come again
+								</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+								<h5>person 3</h5>
+								
+							</div>
+							<div class="clearfix"> </div>
+						</li>
+						<li>
+							
+							<div class="w3layouts_work_grid_right">
+								<h4>
+								
+								Worth to come again
+								</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+								labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+								laboris nisi ut aliquip ex ea commodo consequat. </p>
+								<h5>person 4</h5>
+								
+							</div>
+							<div class="clearfix"> </div>
+						</li>
+					</ul>
+				</div>
+			</section>
+		</div>	
+	</div>
+  
+<section class="contact-w3ls" id="contact">
+	<div class="container">
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile2" data-aos="flip-left">
+			<div class="contact-agileits">
+				<h4>Newsletter</h4>
+				
+				<form  method="post" name="sentMessage" id="contactForm" >
+					<div class="control-group form-group">
+                        
+                            <label class="contact-p1">Name</label>
+                            <input type="text" class="form-control" name="name" id="name" required >
+                            <p class="help-block"></p>
+                       
+                    </div>	
+                    <div class="control-group form-group">
+                        
+                            <label class="contact-p1">Contact No</label>
+                            <input type="tel" class="form-control" name="phone" id="phone" required >
+							<p class="help-block"></p>
+						
+                    </div>
+                    <div class="control-group form-group">
+                        
+                            <label class="contact-p1">Email Id</label>
+                            <input type="email" class="form-control" name="email" id="email" required >
+							<p class="help-block"></p>
+						
+                    </div>
+                    
+                    
+                    <input type="submit" name="sub" value="Submit" class="btn btn-primary">	
+				</form>
+				<?php
+				if(isset($_POST['sub']))
+				{
+					$name =$_POST['name'];
+					$phone = $_POST['phone'];
+					$email = $_POST['email'];
+					$approval = "Not Allowed";
+					$sql = "INSERT INTO `contact`(`fullname`, `phoneno`, `email`,`cdate`,`approval`) VALUES ('$name','$phone','$email',now(),'$approval')" ;
+					
+					
+					if(mysqli_query($con,$sql))
+					echo"OK";
+					
+				}
+				?>
+			</div>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile1" data-aos="flip-right">
+			<h4>Contact Us</h4>
+			<p class="contact-agile1"><strong>Phone :</strong>+91 12343</p>
+			<p class="contact-agile1"><strong>Email :</strong>abc@gmail.com</p>
+			<p class="contact-agile1"><strong>Address :</strong> hyderabad</p>
+																
+			
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</section>
+
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+
+<script src="js/jqBootstrapValidation.js"></script>
+
+
+		<script src="js/jquery-ui.js"></script>
+		<script>
+				$(function() {
+				$( "#datepicker,#datepicker1,#datepicker2,#datepicker3" ).datepicker();
+				});
+		</script>
+
+<link rel="stylesheet" href="css/swipebox.css">
+				
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+
+				<script defer src="js/jquery.flexslider.js"></script>
+				<script type="text/javascript">
+				$(window).load(function(){
+				  $('.flexslider').flexslider({
+					animation: "slide",
+					start: function(slider){
+					  $('body').removeClass('loading');
+					}
+				  });
+				});
+			  </script>
+			
+<script src="js/responsiveslides.min.js"></script>
+			<script>
+						
+						$(function () {
+						 
+						  $("#slider4").responsiveSlides({
+							auto: true,
+							pager: true,
+							nav:false,
+							speed: 500,
+							namespace: "callbacks",
+							before: function () {
+							  $('.events').append("<li>before event fired.</li>");
+							},
+							after: function () {
+							  $('.events').append("<li>after event fired.</li>");
+							}
+						  });
+					
+						});
+			</script>
+		
+		<script src="js/main.js"></script>	
+
+<script src="js/easy-responsive-tabs.js"></script>
+
+<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+</body>
+</html>
+
+
